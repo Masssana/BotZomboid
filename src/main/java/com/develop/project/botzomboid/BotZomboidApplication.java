0b436d89c9220c2b91d0289ccbeec7fcce5f09e5
Masssana <maxikus101@gmail.com>
2025-08-29 17:19:38 +0500

    more_func_methods

---
+++ src/main/java/com/develop/project/botzomboid/BotZomboidApplication.java
@@ -0,0 +1,13 @@
+package com.develop.project.botzomboid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotZomboidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotZomboidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/develop/project/botzomboid/BotZomboidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develop.project.botzomboid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotZomboidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
